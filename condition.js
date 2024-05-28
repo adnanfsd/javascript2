74a1f71d53bd8442d7e284913266c620dfe27b73
adnanfsd <tsadnan39@gmail.com>
2024-05-28 12:16:07 +0530

    commit1

---
+++ condition.js
@@ -0,0 +1,42 @@
+// if(5<10)
+//     {
+//         console.log("5 is lesser");
+//     }
+
+//     else
+//     {
+//         console.log("5 is greater");
+//     }
+
+// (5<10)?console.log("5 is lesser"):console.log("5 is greater");
+
+
+
+
+// 1-11 Good Morning
+// 12-16 Good Afternoon
+// 16-19 Good Evening
+// 19-24 Good Night
+
+// let time=10;
+// if(time>=1 && time<=11){
+//     console.log("Good Morning")
+// }
+// else if(time>=12 && time<16){
+//     console.log("Good Afternoon")
+// }
+// else if(time>=16 && time<19){
+//     console.log("Good Evening")
+// }
+// else{
+//     console.log("Good Night")
+// }
+
+let n=10;
+if(n%2==0)
+    {
+        console.log("Even")
+    }
+    else{
+        console.log("Odd")
+    }
---
+++ loop.js
@@ -0,0 +1,47 @@
+// for(let i=1;i<=5;i++)
+//     {
+//         console.log(i)
+//     }
+
+// for(let i=1;i<=10;i++)
+//          {
+//              console.log(i)
+//          }
+
+// let fruits=["apple","banana","grapes","pineapple"];
+// for(let i=0;i<fruits.length;i++)
+//     {
+//         console.log(fruits[i]);
+//     }
+
+//  find the sum of theory
+// const numbers = [1,2,3,4,5];
+// var sum=0;
+// for(let i=0;i<numbers.length;i++)
+//     {
+//         sum = sum + numbers[i]
+//     }
+// console.log(sum);
+    
+
+// PRINT MULTIPLIACTION OF 1-5
+
+// for(i=1;i<=5;i++)
+//     {
+//         for(n=1;n<=5;n++){
+//             let p = `${i} * ${n} = ${i*n}`
+//             console.log(p)
+//         }
+//     }
+
+let q =[1,4,3,8,6,9,7]
+for (let i=0;i<q.length;i++)
+    {
+        if(q[i]%2==0)
+            {
+                console.log("even")
+            }
+            else{
+                console.log("odd")
+            }
+    }
